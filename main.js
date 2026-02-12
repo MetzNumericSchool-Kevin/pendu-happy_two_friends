40e2b41e50bcc773d441ab1ae21ffbb2f427d0db
Dorian Weber <dorianweb57@gmail.com>
2026-02-12 14:19:14 +0100

    feat(eventListener) : ajout de l'écoute du clavier)

---
+++ main.js
@@ -0,0 +1,5 @@
+//Mettre en place l'écouteur d'événements clavier
+document.addEventListener("keypress", (event) => {
+    // Afficher la lettre dans la zone "Lettres jouées" avec un badge vert
+    console.log(event)
+} )
