b2a65b25f777b882a04d35e3266cae5b83bfb0ac
Dorian Weber <dorianweb57@gmail.com>
2026-02-12 14:50:57 +0100

    feat(duplicata) : vérification de duplicata dans la liste des lettres

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const zoneLettre = document.querySelector("#letters-used");
-
+const lettreJouer = [];
 //Mettre en place l'écouteur d'événements clavier
+//Le joueur tape une lettre sur son clavier physique
 document.addEventListener("keypress", (event) => {
-    // Afficher la lettre dans la zone "Lettres jouées" avec un badge vert
-    zoneLettre.textContent += event["key"].toUpperCase() + " " ;
-    console.log(event);
+    // Une lettre déjà jouée ne peut pas être rejouée
+    if (lettreJouer.includes(event.key)) {
+        alert("Cette Lettre a déjà été jouée.")
+    //Seules les lettres de A à Z sont acceptées
+    } else if (!/^[a-zA-Z]$/.test(event.key)) {
+        alert("Votre saisis n'est pas une lettre.")
+    }else {
+        // Afficher la lettre dans la zone "Lettres jouées" avec un badge vert
+        zoneLettre.textContent += event.key.toUpperCase() + " " ;
+        lettreJouer.push(event["key"])};
 });
