9061bd3acf4531297143d6497c4eff47b0928aa2
Ludvinae <benoit.champlon79@gmail.com>
2026-02-12 15:50:56 +0100

    merged changes

--- main.js
+++ main.js
@@ -33,17 +33,32 @@ const zoneLettre = document.querySelector("#letters-used");
 const lettreJouer = [];
 //Mettre en place l'écouteur d'événements clavier
 //Le joueur tape une lettre sur son clavier physique
-
 document.addEventListener("keypress", (event) => {
     // Une lettre déjà jouée ne peut pas être rejouée
-    const lowerEvent = event.key.toLowerCase()
-    if (lettreJouer.includes(lowerEvent)) {
+    if (lettreJouer.includes(event.key)) {
         alert("Cette Lettre a déjà été jouée.")
     //Seules les lettres de A à Z sont acceptées
-    } else if (!/^[a-zA-Z]$/.test(lowerEvent)) {
+    } else if (!/^[a-zA-Z]$/.test(event.key)) {
         alert("Votre saisis n'est pas une lettre.")
     }else {
         // Afficher la lettre dans la zone "Lettres jouées" avec un badge vert
-        zoneLettre.textContent += lowerEvent + " " ;
-        lettreJouer.push(lowerEvent)};
-})
+        zoneLettre.textContent += event.key.toUpperCase() + " " ;
+        lettreJouer.push(event["key"])};
+        console.log(testLettre(event))
+});
+
+
+
+
+
+
+
+// Tester si la lettre est presente
+function testLettre(lettre) {
+    if (lettre in motATrouver) return true;
+    else false;
+}
+
+// Si la lettre est dans le mot
+let lettreActuelle = lettreJouer[-1];
+console.log(testLettre(lettreActuelle));
